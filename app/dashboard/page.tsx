204e80efc28d0daa5842dd311b84abc09ebc381b
mrs28 <la.reina.bosque@gmail.com>
2024-04-16 19:20:37 -0500

    feat: fetching and streaming done

--- app/dashboard/page.tsx
+++
@@ -1,4 +0,0 @@
-//  Dashboard Page (/dashboard)
-export default function Page() {
-    return <p>Welcome Dashboard Page</p>;
-  }
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -3,7 +3,7 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
+  } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
@@ -13,12 +13,13 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
+
 export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+{/* 
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
